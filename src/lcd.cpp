b04feba4deb16b8e32090163616d4f6820f1078c
stcubing <117993657+stcubing@users.noreply.github.com>
2025-11-14 01:59:19 +1100

    blinking led + lcd testing

---
+++ src/lcd.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+
+
+LiquidCrystal_I2C lcd(0x3F,16,2);
+
+void setup() {
+    lcd.init();
+    lcd.backlight();
+    lcd.print("hello world");
+}
+
+void loop() {
+
+}
